d567fb34cc8e72355274631e528b4b1aa6755658
unknown <ortiztato@gmail.com>
2023-03-03 18:20:12 -0300

    terminada primera version mobile

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
-// script to open and close the nav menu 
+//script to open and close de nav menu
 
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
 const contactbtn = document.querySelector("#contactbtn");
-/* const productosbtn = document.querySelector("#productosbtn"); */
-
+const serviciosbtn = document.querySelector("#serviciosbtn");
+const productosbtn = document.querySelector("#productosbtn");
+const nosotrosbtn = document.querySelector("#nosotrosbtn");
+const logomenubtn = document.querySelector("#logomenubtn");
 
 function toggleMenu() {
   menu.classList.toggle("menu_opened");
@@ -14,4 +16,7 @@ function toggleMenu() {
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
 contactbtn.addEventListener("click", toggleMenu);
-/* productosbtn.addEventListener("click", toggleMenu); */
+serviciosbtn.addEventListener("click", toggleMenu);
+productosbtn.addEventListener("click", toggleMenu);
+nosotrosbtn.addEventListener("click", toggleMenu);
+logomenubtn.addEventListener("click", toggleMenu);
